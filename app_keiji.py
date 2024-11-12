cb61887c990a3644a3a22b728c0e2daec83a0f1e
k024c1035 <k024c1035@g.neec.ac.jp>
2024-11-12 07:04:56 +0000

    [update]ユースケ版に変えた

--- app_keiji.py
+++ app_keiji.py
@@ -6,9 +6,10 @@ def index():
     # テンプレートエンジンにデータを指定
     return render_template(
             'card-age.html',
-            username='ユウスケ',
-            age=20,
-            email='yusuke@example.com')
+            username='ケイジ',
+            age=19,
+            email='keiji@example.com')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
+
